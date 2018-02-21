fbb04a5883be35bf4778e1702cc6505e27a67f88
diegoseso <diegoseso@developer>
2018-02-21 22:11:51 +0100

    Minimal playable version

--- client/assets/states/BootState.js
+++ client/assets/states/BootState.js
@@ -1,8 +1,10 @@
 var BootState = {
    init: function(){
            this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
-           this.scale.pageAlignHorizontally = true
-           this.scale.pageAlignVertically = true
+           this.scale.maxHeight = 640;
+           this.scale.maxWidth = 360;
+           this.scale.pageAlignHorizontally = true;
+           this.scale.pageAlignVertically = true;
        },
    preload: function(){
        this.load.image('logo', 'images/cover_360_640.png');
@@ -10,8 +12,6 @@ var BootState = {
        this.load.image('snake', 'images/head.png');
        this.load.image('snakePoop', 'images/poison.png');
        this.load.image('food', 'images/coin_front.png');
-       //this.load.audio('coin', './coin.mp3');
-       this.load.audio('cheering', './cheering.mp3');
    },
    create: function(){
        this.state.start('PreloadState');
--- client/assets/states/GameState.js
+++ client/assets/states/GameState.js
@@ -1,81 +1,107 @@
+var snake, food, cursors, score = 0;
+var gameWidth = 360
+var gameHeight = 640
+var snakePoop = [];
+var gameOver = false;
+var velocitySpeed = 200;
+
+function addTail(context) {
+    coinSound.play()
+    this.score++
+    console.log("x:" + snake.body.velocity.x);
+    console.log("y:" + snake.body.velocity.y);
+
+    var spawnOffsetX = 0;
+    var spawnOffsetY = 0;
+
+    if (snake.body.velocity.x > 0) {
+        spawnOffsetX = -20
+    }
+
+    if (snake.body.velocity.x < 0) {
+        spawnOffsetX = 20
+    }
+
+    if (snake.body.velocity.Y > 0) {
+        spawnOffsetY = -20
+    }
+
+    if (snake.body.velocity.y < 0) {
+        spawnOffsetY = 20
+    }
+
+    scaledPoop = context.add.sprite()
+
+    poop = context.add.sprite(snake.x + spawnOffsetX, snake.y + spawnOffsetY, 'snakePoop');
+    poop.enableBody = true;
+    poop.scale.setTo(0.03, 0.03);
+
+    snakePoop.push(poop);
+    this.snakeTail = snakePoop[snakePoop.length - 1]
+    this.snakeTail.scale.x = 0
+    this.snakeTail.scale.y = 0
+    context.add.tween(this.snakeTail.scale).to({x: 0.03, y: 0.03}, 50, Phaser.Easing.Linear.None, true);
+    context.physics.arcade.enable(this.snakeTail);
+}
+
+function foodCollisionHandler() {
+    var y = GameState.world.randomY;
+    var x = GameState.world.randomX;
+    if (x < 40) {
+        x = 40
+    } else if (x > this.gameWidth - 40) {
+        x = this.gameWidth - 40
+    }
+
+    if (y < 40) {
+        y = 40
+    } else if (y > this.gameHeight - 40) {
+        y = this.gameHeight - 40
+    }
+    food.reset(x, y);
+    this.velocitySpeed = this.velocitySpeed + 50;
+    addTail(this);
+}
+
+
+function poopCollisionHandler() {
+    stateText.visible = true;
+    food.destroy();
+    //ga('send', 'event', "Game", "HightScore", "GameOver", score);
+}
+
 var GameState = {
-    init: function(){
+    preload: function(){
     },
     create: function () {
-    console.log("The game has started");
-        console.log('hello, its me ');
-        var snake, food, cursors, score = 0;
-        var snakePoop = [];
-        var gameOver = false;
-        var velocitySpeed = 200;
-        var gameWidth = 360;
-        var gameHeight = 640;
 
-        function foodCollisionHandler () {
-            var y = this.world.randomY;
-            var x = this.world.randomX;
-            if (x < 40) {
-                x = 40
-            } else if ( x > gameWidth - 40 ){
-                x = gameWidth - 40
-            }
-
-            if (y < 40) {
-                y = 40
-            } else if ( y > gameHeight - 40 ){
-                y = gameHeight - 40
-            }
-            food.reset(x, y);
-            velocitySpeed = velocitySpeed + 50;
-            addTail(this);
+        this.physics.startSystem(Phaser.Physics.ARCADE);
+        snake = this.add.sprite(100, 100, 'snake');
+        snake.enableBody = true;
+        snake.scale.setTo(0.03, 0.03);
+        coinSound = this.add.audio('coin');
+        cheeringSound = this.add.audio('cheering');
+        var y = this.world.randomY;
+        var x = this.world.randomX;
+        if (x < 40) {
+            x = 40
+        } else if ( x > gameWidth - 40 ){
+            x = gameWidth - 40
         }
 
-        function addTail(context) {
-            coinSound.play()
-            score++
-            console.log("x:" + snake.body.velocity.x);
-            console.log("y:" + snake.body.velocity.y);
-
-            var spawnOffsetX = 0;
-            var spawnOffsetY = 0;
-
-            if (snake.body.velocity.x > 0) {
-                spawnOffsetX =  - 20
-            }
-
-            if (snake.body.velocity.x < 0) {
-                spawnOffsetX = 20
-            }
-
-            if (snake.body.velocity.Y > 0) {
-                spawnOffsetY = - 20
-            }
-
-            if (snake.body.velocity.y < 0) {
-                spawnOffsetY = 20
-            }
-
-            poop = this.add.sprite(snake.x + spawnOffsetX, snake.y + spawnOffsetY, 'snakePoop');
-
-            snakePoop.push(poop);
-            this.snakeTail = snakePoop[snakePoop.length - 1]
-            this.snakeTail.scale.x = 0
-            this.snakeTail.scale.y = 0
-            this.add.tween(this.snakeTail.scale).to( { x: 1, y: 1 }, 2000, Phaser.Easing.Linear.None, true);
-            this.physics.arcade.enable(this.snakeTail);
+        if (y < 40) {
+            y = 40
+        } else if ( y > gameHeight - 40 ){
+            y = gameHeight - 40
         }
-
-
-        function poopCollisionHandler() {
-            console.log('💩💩💩you touch the poop💩💩💩');
-            console.log('💩💩💩Game Over💩💩💩');
-            cheeringSound.play();
-
-            stateText.visible = true;
-
-            food.destroy();
-            ga('send', 'event', "Game", "HightScore", "GameOver", score);
-        }
+        food = this.add.sprite(x, y, 'food');
+        food.enableBody = true;
+        food.scale.setTo(0.03, 0.03);
+        this.physics.arcade.sortDirection = Phaser.Physics.Arcade.BOTTOM_TOP;
+        this.physics.arcade.enable(snake);
+        this.physics.arcade.enable(food);
+        this.physics.arcade.enable(snakePoop);
+        cursors = this.input.keyboard.createCursorKeys();
 
         function snakeOut(snake) {
             if (snake.x > 600) {
@@ -96,76 +122,46 @@ var GameState = {
                 snake.reset(snake.x, 380);
                 snake.body.velocity.y = -velocitySpeed;
             }
+
         }
 
-    this.physics.startSystem(Phaser.Physics.ARCADE);
-    snake = this.add.sprite(100, 100, 'snake');
-    snake.enableBody = true;
-    snake.scale.setTo(0.03, 0.03);
-    coinSound = this.add.audio('coin');
-    cheeringSound = this.add.audio('cheering');
-    var y = this.world.randomY;
-    var x = this.world.randomX;
-    if (x < 40) {
-        x = 40
-    } else if ( x > gameWidth - 40 ){
-        x = gameWidth - 40
-    }
+        snake.checkWorldBounds = true;
+        snake.events.onOutOfBounds.add(snakeOut, this);
 
-    if (y < 40) {
-        y = 40
-    } else if ( y > gameHeight - 40 ){
-        y = gameHeight - 40
-    }
-    food = this.add.sprite(x, y, 'food');
-    food.scale.setTo(0.3, 0.3);
-    this.physics.arcade.sortDirection = Phaser.Physics.Arcade.BOTTOM_TOP;
-    this.physics.arcade.enable(snake);
-    this.physics.arcade.enable(food);
-    this.physics.arcade.enable(snakePoop);
-    cursors = this.input.keyboard.createCursorKeys();
+        stateText = this.add.text(this.world.centerX,this.world.centerY,'Game Over!', { font: '84px Arial', fill: '#fff' });
+        stateText.anchor.setTo(0.5, 0.5);
+        stateText.visible = false;
 
-    snake.checkWorldBounds = true;
-    snake.events.onOutOfBounds.add(snakeOut, this);
-
-    stateText = this.add.text(this.world.centerX,this.world.centerY,'Game Over!', { font: '84px Arial', fill: '#fff' });
-    stateText.anchor.setTo(0.5, 0.5);
-    stateText.visible = false;
-
-    scoreText = this.add.text(this.world.centerX,this.world.centerY,'Score: ' + score, { font: '22px Arial', fill: '#fff' });
-    scoreText.x = 0;
-    scoreText.y = 0;
+        scoreText = this.add.text(this.world.centerX,this.world.centerY,'Score: ' + score, { font: '22px Arial', fill: '#fff' });
+        scoreText.x = 0;
+        scoreText.y = 0;
     },
     update: function () {
 
-    scoreText.text = 'Score: ' + this.score
-    this.physics.arcade.collide(this.snake, food, foodCollisionHandler, null, this);
+        scoreText.text = 'Score: ' + score
+        this.physics.arcade.collide(snake, food, foodCollisionHandler, null, this);
 
-    if (!gameOver) {
-        if (cursors.left.isDown)
-        {
-            snake.body.velocity.x = -velocitySpeed;
-            snake.body.velocity.y = 0;
+        if (!this.gameOver) {
+            if (cursors.left.isDown) {
+                snake.body.velocity.x = -velocitySpeed;
+                snake.body.velocity.y = 0;
+            }
+            else if (cursors.right.isDown) {
+                snake.body.velocity.x = velocitySpeed;
+                snake.body.velocity.y = 0;
+            }
+            if (cursors.up.isDown) {
+                snake.body.velocity.x = 0;
+                snake.body.velocity.y = -velocitySpeed;
+            }
+            else if (cursors.down.isDown) {
+                snake.body.velocity.x = 0;
+                snake.body.velocity.y = velocitySpeed;
+            }
         }
-        else if (cursors.right.isDown)
-        {
-            snake.body.velocity.x = velocitySpeed;
-            snake.body.velocity.y = 0;
-        }
-        if (cursors.up.isDown)
-        {
-            snake.body.velocity.x = 0;
-            snake.body.velocity.y = -velocitySpeed;
-        }
-        else if (cursors.down.isDown)
-        {
-            snake.body.velocity.x = 0;
-            snake.body.velocity.y = velocitySpeed;
+
+        for (var i = 0; i < snakePoop.length; i++) {
+            this.physics.arcade.collide(snake, snakePoop[i], poopCollisionHandler, null, this)
         }
     }
-
-    for (var i = 0; i < snakePoop.length; i++) {
-        this.physics.arcade.collide(snake, snakePoop[i], poopCollisionHandler, null, this)
-    }
-}
-}
+};
